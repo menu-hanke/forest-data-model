f4b95bbed3784be9e188802414c9c0df3a8a6c79
Niemelä Urho (LUKE) <urho.niemela@luke.fi>
2022-05-17 17:43:07 +0300

    test: add species conversion tests

--- tests/species_conversion_test.py
+++ tests/species_conversion_test.py
@@ -1,9 +1,22 @@
+from forestdatamodel.enums.internal import TreeSpecies
+from forestdatamodel.enums.vmi import VmiSpecies
 from tests import test_util
 from forestdatamodel.converters import species_conversions
+
+# testaa myös että väärä koodi palauttaa virheen
+
 
 class TestConversion(test_util.ConverterTestSuite):
     def test_convert_vmi_species_to_internal_species(self):
         assertions = [
-            (["A1"], 12)
+            (["A1"], TreeSpecies.SHORE_PINE)
         ]
-        self.run_with_test_assertions(assertions, species_conversions.vmi_to_internal)
+        self.run_with_test_assertions(
+            assertions, species_conversions.vmi_to_internal)
+
+    def test_convert_fc_species_to_internal_species(self):
+        assertions = [
+            ([1], TreeSpecies.PINE)
+        ]
+        self.run_with_test_assertions(
+            assertions, species_conversions.fc_to_internal)
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,8 +1,14 @@
 import typing
 import unittest
 
+
 class ConverterTestSuite(unittest.TestCase):
-    def run_with_test_assertions(self, assertions: typing.List[typing.Tuple], fn:typing.Callable):
+    def run_with_test_assertions(self, assertions: typing.List[typing.Tuple], fn: typing.Callable):
         for case in assertions:
             result = fn(*case[0])
             self.assertEqual(case[1], result)
+
+    def assertions_should_raise_ValueError(self, assertions: typing.List[typing.Tuple], fn: typing.Callable):
+        for case in assertions:
+            result = fn(*case[0])
+            self.assertRaises(ValueError)
