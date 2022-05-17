6af13d350752bf23a3b8a89429feb06481bc32f6
Niemelä Urho (LUKE) <urho.niemela@luke.fi>
2022-05-17 17:42:42 +0300

    feat: add tree species conversion logic

--- forestdatamodel/converters/species_conversions.py
+++ forestdatamodel/converters/species_conversions.py
@@ -1,30 +1,15 @@
-from forestdatamodel.constants.forest_centre_const import ForestCentreSpecies
-from forestdatamodel.constants.vmi_const import VMISpecies
-from forestdatamodel.constants.internal_const import InternalTreeSpecies
-
-# use the mapping.py to get the converted value
-
-# def vmi_to_internal(vmicode: str) -> InternalTreeSpecies:
-#     try:
-#         vmi_species_name = VMISpecies(vmicode).name
-#         # getting the INternalTreeSpecies at "SHORE_PINE" doesn't make sense - need to access by value, not key/name
-#         internal_species = InternalTreeSpecies[vmi_species_name]
-#         return internal_species
-
-#     except Exception as e:
-#         print(f"could not convert vmi code {vmicode} to internal code")
-#         raise
+from forestdatamodel.enums.forest_centre import ForestCentreSpecies
+from forestdatamodel.enums.vmi import VmiSpecies
+from forestdatamodel.enums.mela import MelaTreeSpecies
+from forestdatamodel.enums.internal import TreeSpecies
+from forestdatamodel.converters import mapping
 
 
-# def fc_to_internal(fc_code: int) -> InternalTreeSpecies:
-#     try:
-#         fc_species_name = ForestCentreSpecies(fc_code).name
-#         internal_species = InternalTreeSpecies(fc_species_name)
-#         return internal_species
-#     except Exception as e:
-#         print(
-#             f"could not convert forest centre code {fc_species_name} to internal code")
-#         raise
+def vmi_to_internal(vmi_code: str) -> TreeSpecies:
+    vmi_species = VmiSpecies(vmi_code)
+    return mapping.vmi_to_internal[vmi_species]
 
 
-# testaa että väärä koodi palauttaa virheen
+def fc_to_internal(fc_code: int) -> TreeSpecies:
+    fc_species = ForestCentreSpecies(fc_code)
+    return mapping.fc_to_internal[fc_species]
