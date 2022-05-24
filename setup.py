76f236056c946816a456864eb7be62090cc5b075
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-05-24 11:48:04 +0300

    chore: set version 0.2.1

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="forestdatamodel",
     description="Data classes and utilities for forest stand, tree strata and reference trees representation",
-    version="0.2.0",
+    version="0.2.1",
     packages=["forestdatamodel"]
 )
